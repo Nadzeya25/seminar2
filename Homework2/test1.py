ac1a962be76379de9cfbb583bbfb2b5a117e912e
Nadzeya <lustra2013@gmail.com>
2023-09-27 21:24:14 +0300

    добавлен комментарий

--- Homework2/test1.py
+++ Homework2/test1.py
@@ -1,3 +1,5 @@
+# вспоминаю, как работать со списками
+
 list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 list2 = [10, 11, 12, 13, 14, 15]
 list3 = ['a', 'b', 'c', 'd', 'e', 'f']
@@ -5,4 +7,4 @@ list3 = ['a', 'b', 'c', 'd', 'e', 'f']
 current_num = 14
 print(list2.index(current_num))  # 4
 print(list2[4])  # 14
-print(list3[list2.index(current_num)])
+print(list3[list2.index(current_num)])  # e
